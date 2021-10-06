7c945e5a059822954a3153bc5e198069e6b0deed
ljpop <80583587+ljpop@users.noreply.github.com>
2021-10-06 17:42:08 +0200

    JavaScript done

--- main.js
+++ main.js
@@ -1,14 +1,57 @@
-const username = "ljpop";
-const GIT_URL = fetch("https://api.github.com/users/" + username);
+//---------Functions----------------------------------------//
+const formatDate = function (date) {
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  return new Date(date).toLocaleDateString("en-GB", options);
+};
 
-const promise = GIT_URL;
+const getText = function (id, data, error) {
+  if (data) document.getElementById(id).textContent = data;
+  else document.getElementById(id).textContent = error;
+};
 
-promise
-  .then(function (response) {
-    const processingPromise = response.json();
-    return processingPromise;
-  })
-  .then(function (processedResponse) {
-    console.log(processedResponse);
-    console.log(processedResponse.login);
-  });
+//---------------------------------------------------------//
+
+let username = "ljpop";
+
+//----------Form----------------------------------//
+const doSearch = function (event) {
+  event.preventDefault();
+
+  username = document.getElementById("username").value;
+  const GIT_URL = "https://api.github.com/users/" + username;
+  const promise = fetch(GIT_URL);
+
+  promise
+    .then(function (response) {
+      if (!response.ok) return false;
+      else {
+        const processingPromise = response.json();
+        return processingPromise;
+      }
+    })
+    .then(function (processedResponse) {
+      if (!processedResponse) {
+        console.log("not ok");
+        document.querySelector(".errorMessage").classList.add("visible");
+      } else {
+        document.querySelector(".errorMessage").classList.remove("visible");
+        const data = processedResponse;
+        document.getElementById("avatar").src = data.avatar_url;
+        getText("name", data.name);
+        getText("login", `@${data.login}`);
+        getText("company", data.company, "Not Available");
+        getText("date_created", formatDate(data.created_at));
+        getText("reposNum", data.public_repos, "0");
+        getText("followers", data.followers, "0");
+        getText("following", data.following, "0");
+        getText("location", data.location, "Not Available");
+        getText("blog", data.blog, "Not Available");
+        getText("bio", data.bio, "This profile has no bio");
+        getText("twitter", data.twitter_username, "Not Available");
+      }
+    });
+};
