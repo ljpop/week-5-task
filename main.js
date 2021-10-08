1c7dd52174f8c89c6e37d28a623250f6e9ee7f22
ljpop <80583587+ljpop@users.noreply.github.com>
2021-10-08 11:02:03 +0200

    started working on darkmode

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+//---------Light-Mode----------------------------------------//
+let darkMode = false;
+
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  darkModeProperties();
+}
+
+document.querySelector(".light-mode").addEventListener("click", function () {
+  if (darkMode == false) {
+    darkModeProperties();
+  } else {
+    lightModeProperties();
+  }
+});
+
+function darkModeProperties() {
+  document.querySelector("body").classList.add("darkmode");
+  document.querySelector(".statistics").classList.add("darkmode");
+  document.querySelectorAll(".number-stat").classList.add("darkmode"); /*TODO:*/
+  // root.setProperty("--lm-bg", "#141D2F");
+  // root.setProperty("--lm-bg-content", "#1E2A47");
+  // root.setProperty("--lm-text", "white");
+  // root.setProperty("--lm-text-alt", "white");
+  // root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+  // modetext.innerText = "LIGHT";
+  // modeicon.src = "./assets/icon-sun.svg";
+  // root.setProperty("--lm-icon-bg", "brightness(1000%)");
+  darkMode = true;
+}
+function lightModeProperties() {
+  // root.setProperty("--lm-bg", "#F6F8FF");
+  // root.setProperty("--lm-bg-content", "#FEFEFE");
+  // root.setProperty("--lm-text", "#4B6A9B");
+  // root.setProperty("--lm-text-alt", "#2B3442");
+  // root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+  // modetext.innerText = "DARK";
+  // modeicon.src = "./assets/icon-moon.svg";
+  // root.setProperty("--lm-icon-bg", "brightness(100%)");
+  darkMode = false;
+}
+
 //---------Functions----------------------------------------//
 const formatDate = function (date) {
   const options = {
@@ -11,6 +55,17 @@ const formatDate = function (date) {
 const getText = function (id, data, error) {
   if (data) document.getElementById(id).textContent = data;
   else document.getElementById(id).textContent = error;
+};
+
+const removeError = function () {
+  document.querySelector(".errorMessage").classList.remove("visible");
+};
+
+const addError = function () {
+  console.log("not ok");
+  document.querySelector(".errorMessage").classList.add("visible");
+  document.getElementById("username").placeholder = "";
+  document.getElementById("username").value = "";
 };
 
 //----------Form----------------------------------//
@@ -31,10 +86,9 @@ const doSearch = function (event) {
     })
     .then(function (processedResponse) {
       if (!processedResponse) {
-        console.log("not ok");
-        document.querySelector(".errorMessage").classList.add("visible");
+        addError();
       } else {
-        document.querySelector(".errorMessage").classList.remove("visible");
+        removeError();
         const data = processedResponse;
         document.getElementById("avatar").src = data.avatar_url;
         getText("name", data.name);
